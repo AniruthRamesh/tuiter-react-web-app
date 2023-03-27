c253dd386b7f4c306b2a5a24811b8ed4490d5ca1
AniruthRamesh <anikrishna3@gmail.com>
2023-03-27 19:55:05 -0400

    assignment 8 node sp23 changes

--- src/tuiter/Tuits/TuitStats.js
+++ src/tuiter/Tuits/TuitStats.js
@@ -8,51 +8,21 @@ const TuitStats = ({tuit,id,likes,comments,retweets,liked})=>{
     const dispatch = useDispatch()
 
 
-    const [like,setLike] = useState(liked)
-    const [dislike,setDisLike] = useState(tuit.disliked)
-
     const handleLikeClick = ()=>{
-        const toggle = !like
-        setLike(toggle)
-
-        if(!like){
-            console.log("calling update")
             dispatch(updateTuitThunk({
                 ...tuit,
                 likes: likes + 1
               }))
          
-        }
-
-        if(like){
-            dispatch(updateTuitThunk({
-                ...tuit,
-                likes: likes - 1
-              }))
-        }
     }
 
 
     const handleDisLikeClick = ()=>{
-        const toggle = !dislike
-        setDisLike(toggle)
-
-        if(!dislike){
-            console.log("calling update")
             dispatch(updateTuitThunk({
                 ...tuit,
                 dislikes: tuit.dislikes + 1,
                 
               }))
-         
-        }
-
-        if(dislike){
-            dispatch(updateTuitThunk({
-                ...tuit,
-                dislikes: tuit.dislikes - 1
-              }))
-        }
     }
 
 
@@ -68,13 +38,13 @@ const TuitStats = ({tuit,id,likes,comments,retweets,liked})=>{
                 <span className="text-secondary fs-6"><p className="d-inline ms-2">{retweets}</p></span>
                 </span></div>
             <div className="col"><span><Link to=""
-                ><i className={like? "bi bi-heart-fill" : "bi bi-heart"} style={{color: like? "red":"gray"}} 
+                ><i className={"bi bi-heart-fill"} style={{color:"red"}} 
                 onClick={handleLikeClick}></i></Link>
                 <span className="text-secondary fs-6"><p className="d-inline ms-2">{likes}</p></span>
                 </span></div>
 
             <div className="col"><span><Link to=""
-                ><i className={!dislike? "bi bi-hand-thumbs-down" : "bi bi-hand-thumbs-down-fill"} style={{color: dislike? "red":"gray"}} 
+                ><i className={ "bi bi-hand-thumbs-down-fill"} style={{color:"red"}} 
                 onClick={handleDisLikeClick}></i></Link>
                 <span className="text-secondary fs-6"><p className="d-inline ms-2">{tuit.dislikes}</p></span>
                 </span></div>
